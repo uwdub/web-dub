7c140b210725a485c23c4a87ddcd98f2623f96ae
James Fogarty <jayfo.github@gmail.com>
2014-10-18 01:05:22 -0700

    Test Author ID Uniqueness

--- tests/test_authors.py
+++ tests/test_authors.py
@@ -1,3 +1,4 @@
+import re
 import unittest
 import yaml
 
@@ -42,3 +43,33 @@ class TestAuthors(unittest.TestCase):
                 id_expected,
                 '{} does not have expected id {}'.format(id_author, id_expected)
             )
+
+    def test_authors_id_unique(self) -> None:
+        """
+        Confirm every author id is unique.
+
+        The YAML parser does not error on this, and just keeps the most recently parsed.
+
+        So we need to look at the file ourself.
+        """
+        with open('_data/authors.yml') as f:
+            id_existing = set()
+            for line in f:
+                match = re.match('(id_author_.*):', line)
+                if match:
+                    id_author = match.group(1)
+
+                    self.assertNotIn(
+                        id_author,
+                        id_existing,
+                        '{} is duplicated in authors.yml'.format(id_author)
+                    )
+
+                    id_existing.add(id_author)
+
+            self.assertGreater(
+                len(id_existing),
+                0,
+                'No ID were parsed in authors.yml'
+            )
+
