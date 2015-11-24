31e989671e7a09b3b6661e6bc714555d10f685fb
James Fogarty <jayfo.github@gmail.com>
2015-11-23 20:29:48 -0800

    Fix Unicode in Invoke

--- tasks.py
+++ tasks.py
@@ -1,12 +1,20 @@
 import invoke
+import sys
 
 
 @invoke.task
 def update_dependencies():
-    invoke.run('gem install bundler')
-    invoke.run('bundle install')
-    invoke.run('npm install')
-    invoke.run('pip install -r requirements3.txt')
+    # The npm install command sometimes outputs characters that cause Unicode
+    # errors on Windows, so we hide the output and print our own simple messages
+    #
+    # Setting the encoding parameter also works, done here in case the hide is removed
+    print('Updating Node.js dependencies')
+    invoke.run('npm install', encoding=sys.stdout.encoding, hide=True)
+    print('Updating Python dependencies')
+    invoke.run('pip install -r requirements3.txt', hide=True)
+    print('Updating Ruby dependencies')
+    invoke.run('gem install bundler', hide=True)
+    invoke.run('bundle install', hide=True)
 
 
 @invoke.task(pre=[update_dependencies])
@@ -16,7 +24,9 @@ def build():
 
 @invoke.task(pre=[update_dependencies])
 def serve():
-    invoke.run('bundle exec jekyll serve -t --config _config.yml,_config-serve.yml --watch --force_polling --incremental')
+    invoke.run(
+        'bundle exec jekyll serve -t --config _config.yml,_config-serve.yml --watch --force_polling --incremental'
+    )
 
 
 @invoke.task
