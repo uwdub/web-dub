9cd47225bd0f736a905ecd8305543f503e2f76c0
James Fogarty <jayfo.github@gmail.com>
2016-01-01 09:23:36 -0800

    Fix Javascript Function Wrappers

--- js/base/bar-sidebar-right.js
+++ js/base/bar-sidebar-right.js
@@ -1,15 +1,15 @@
 // https://stackoverflow.com/questions/20671303/bootstrap_3-changing-div-order-on-small-screens-only/20672040#20672040
 
-function base_sidebar_right_position_remainder() {
-  var w = $(window).width();
-  if (w < 992 && $('#base-sidebar-right-remainder-tucked').children().length > 0) {
-    $('#base-sidebar-right-remainder-content').prependTo( $('#base-sidebar-right-remainder-bottom') );
-  } else if (w > 992 && $('#base-sidebar-right-remainder-bottom').children().length > 0) {
-    $('#base-sidebar-right-remainder-content').prependTo( $('#base-sidebar-right-remainder-tucked') );
-  }
-}
-
 $(function() {
+  function base_sidebar_right_position_remainder() {
+    var w = $(window).width();
+    if (w < 992 && $('#base-sidebar-right-remainder-tucked').children().length > 0) {
+      $('#base-sidebar-right-remainder-content').prependTo( $('#base-sidebar-right-remainder-bottom') );
+    } else if (w > 992 && $('#base-sidebar-right-remainder-bottom').children().length > 0) {
+      $('#base-sidebar-right-remainder-content').prependTo( $('#base-sidebar-right-remainder-tucked') );
+    }
+  }
+
   $(window).ready(base_sidebar_right_position_remainder);
   $(window).resize(base_sidebar_right_position_remainder);
 });
