69469d0d1f9c76bc6c25371a4f8941ce6cab8f3b
James Fogarty <jayfo.github@gmail.com>
2016-06-11 06:07:17 -0700

    Clean up lessjs.rb

--- _plugins/base/lessjs.rb
+++ _plugins/base/lessjs.rb
@@ -1,15 +1,16 @@
+# Based in large part on this:
+#
 # https://gist.github.com/andyfowler/642739
-
+#
+# Expects a lessc: key in _config.yml file with the path to a local less.js/bin/lessc
+# Less.js will require node.js to be installed
 module Jekyll
-  
   class LessCssFile < StaticFile
     def write(dest)
       # do nothing
     end
   end
   
-# Expects a lessc: key in your _config.yml file with the path to a local less.js/bin/lessc
-# Less.js will require node.js to be installed
   class LessJsGenerator < Generator
     safe true
     priority :low
@@ -22,7 +23,7 @@ module Jekyll
       raise "Missing 'lessc' path in site configuration" if !site.config['lessc']
       
       # static_files have already been filtered against excludes, etc.
-      generated = []
+      generated_from = []
       site.static_files.each do |sf|
         next if not sf.path =~ less_ext
         
@@ -47,14 +48,15 @@ module Jekyll
           raise "LESS compilation error" if $?.to_i != 0
         end
 
-        # Track what we generated
-        generated << sf
+        # Track the files we used to generate, so we can delete them later
+        generated_from << sf
 
         # Add this output file so it won't be cleaned
         site.static_files << LessCssFile.new(site, site.source, css_dir_relative, css_name)
       end
 
-      site.static_files = site.static_files - generated
+      # Now remove the less files
+      site.static_files = site.static_files - generated_from
     end
   end
 end
