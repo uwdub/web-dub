f026a794c9fb0153250a4ad9fa8e7418162fbf4a
James Fogarty <jayfo.github@gmail.com>
2016-06-26 06:25:55 -0700

    Improve Output and Error Checking in update_dependencies

--- tasks.py
+++ tasks.py
@@ -5,80 +5,121 @@ import sys
 import yaml
 
 
+def check_result(result, description):
+    if result.failed:
+        print('========================================')
+        print('Failed to {}'.format(description))
+        print('')
+        print('========================================')
+        print('STDOUT:')
+        print('========================================')
+        print(result.stdout)
+        print('========================================')
+        print('STDERR:')
+        print('========================================')
+        print(result.stderr)
+        print('========================================')
+        raise Exception('Failed to {}'.format(description))
+
+
 @invoke.task
 def update_dependencies():
+    # Parameters to keep everything silent
+    params_silent = {
+        'encoding': sys.stdout.encoding,
+        'hide': 'both',
+        'warn': True
+    }
+
     # Parse our compile config
     with open('_compile-config.yml') as f:
-        compile_config = yaml.safe_load(f)
+        compile_config_yaml = yaml.safe_load(f)
 
     # The npm install command sometimes outputs characters that cause Unicode
     # errors on Windows, so we set the encoding parameter on our commands
-    print('Updating Node.js dependencies')
-    invoke.run('npm install', encoding=sys.stdout.encoding)
+    print('Checking Node.js dependencies')
+    result = invoke.run('npm install', **params_silent)
+    check_result(result, 'install Node.js dependencies')
 
     # Python dependencies
-    print('Updating Python dependencies')
+    print('Checking Python dependencies')
 
     # Ensure we have a current version of pip, as needed by pip-tools
-    version_desired = compile_config['config']['pip_version']
-    result = invoke.run('pip --disable-pip-version-check show pip', encoding=sys.stdout.encoding, warn=True)
-    version_current = re.search('^Version: ([\d\.]+)', result.stdout, re.MULTILINE).group(1)
-    if version_current != version_desired:
-        invoke.run(
-            'python -m pip install --upgrade pip=={}'.format(
-                version_desired
-            ),
-            encoding=sys.stdout.encoding
-        )
+    pip_version_desired = compile_config_yaml['config']['pip_version']
+    result = invoke.run('pip --disable-pip-version-check show pip', **params_silent)
+    check_result(result, 'check pip version')
 
-    # Ensures we have pip-tools
-    # pip-sync also does not respect options in the requirements file, so this ensures pip-sync only needs to delete
-    invoke.run('pip --disable-pip-version-check install -r requirements3.txt', encoding=sys.stdout.encoding)
+    pip_version_current = re.search('^Version: ([\d\.]+)', result.stdout, re.MULTILINE).group(1)
+    if pip_version_current != pip_version_desired:
+        result = invoke.run(
+            'python -m pip install --upgrade pip=={}'.format(
+                pip_version_desired
+            ),
+            **params_silent
+        )
+        check_result(result, 'update pip version')
+
+    # Ensures we have pip-tools, which will be in our requirements file
+    # pip-sync also does not respect options in the requirements file,
+    # so installing the entire file ensures pip-sync only needs to delete
+    result = invoke.run('pip --disable-pip-version-check install -r requirements3.txt', **params_silent)
+    check_result(result, 'install pip requirements')
     # Ensure we have exactly our dependencies
-    invoke.run('pip-sync requirements3.txt', encoding=sys.stdout.encoding)
+    result = invoke.run('pip-sync requirements3.txt', **params_silent)
+    check_result(result, 'sync pip requirements')
 
     # Ruby dependencies
-    print('Updating Ruby dependencies')
+    print('Checking Ruby dependencies')
 
     # Check we have the correct Bundler version
-    result = invoke.run('gem list -i bundler -v 1.10.6', encoding=sys.stdout.encoding, warn=True)
+    bundler_version_desired = compile_config_yaml['config']['bundler_version']
+
+    result = invoke.run('gem list -i bundler -v {}'.format(bundler_version_desired), **params_silent)
+    # expected to fail if the desired version is not installed
     if result.failed:
-        invoke.run('gem install bundler -v 1.10.6', encoding=sys.stdout.encoding)
+        result = invoke.run('gem install bundler -v {}'.format(bundler_version_desired), **params_silent)
+        check_result(result, 'install bundler')
 
     # And only the correct Bundler version
-    invoke.run('gem uninstall bundler -v "!=1.10.6"', encoding=sys.stdout.encoding, warn=True)
-
-    # List our bundler installs
-    invoke.run('gem list bundler', encoding=sys.stdout.encoding, warn=True)
+    invoke.run('gem uninstall bundler -v "!={}"'.format(bundler_version_desired), **params_silent)
+    # expected to fail if no other versions of bundler are installed
 
     # Check we have our Ruby dependencies
-    result = invoke.run('bundle check', encoding=sys.stdout.encoding, warn=True)
+    result = invoke.run('bundle check', **params_silent)
+    # expected to fail if we are missing dependencies
     if result.failed:
-        invoke.run('bundle install', encoding=sys.stdout.encoding)
+        result = invoke.run('bundle install', **params_silent)
+        check_result(result, 'bundle install')
 
 
 @invoke.task(pre=[update_dependencies])
 def build_production():
-    invoke.run('bundle exec jekyll build -t --config _config.yml,_config-build-production.yml', encoding=sys.stdout.encoding)
+    invoke.run(
+        'bundle exec jekyll build -t --config _config.yml,_config-build-production.yml',
+        encoding=sys.stdout.encoding
+    )
 
 
 @invoke.task(pre=[update_dependencies])
 def build_test():
-    invoke.run('bundle exec jekyll build -t --config _config.yml,_config-build-test.yml', encoding=sys.stdout.encoding)
+    invoke.run(
+        'bundle exec jekyll build -t --config _config.yml,_config-build-test.yml',
+        encoding=sys.stdout.encoding
+    )
 
 
 @invoke.task()
 def compile_config():
     # Parse our compile config
     with open('_compile-config.yml') as f:
-        compile_config = yaml.safe_load(f)
+        compile_config_yaml = yaml.safe_load(f)
 
     # Compile each jinja2 file
-    for jinja2_entry in compile_config['jinja2']['entries']:
+    for jinja2_entry in compile_config_yaml['jinja2']['entries']:
         jinja2_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(searchpath='.'))
         template = jinja2_environment.get_template(jinja2_entry['in'])
         with open(jinja2_entry['out'], 'w') as f:
-            f.write(template.render(compile_config['config']))
+            f.write(template.render(compile_config_yaml['config']))
 
 
 @invoke.task(pre=[update_dependencies])
