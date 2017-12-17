313d067b60c8bd2f235b8dac9f8e51e768d9df14
goldpbear <tcroxson@gmail.com>
2017-12-16 20:46:00 -0800

    refactor(faculty profiles) use google API to fetch faculty info
    - add a dependency on google-api-python-client

--- base/invoke/tasks/build-faculty-profiles.py
+++ base/invoke/tasks/build-faculty-profiles.py
@@ -1,8 +1,10 @@
 #
 # build-faculty-profiles.py
 #
-# Convert a CSV file of faculty information to Markdown files appropriate for
-# consumption by Jekyll.
+# Fetch a CSV file of faculty bio information from Google Drive and convert it
+# to Markdown files suitable for consumption by Jekyll.
+#
+# usage: invoke build_faculty_profiles.py
 #
 
 # NOTE: This script is very sensitive to the naming of fields in Google Forms.
@@ -15,45 +17,83 @@
 #       this is possible in Google Forms.
 
 import invoke
-import csv
-import requests
-import imghdr
 import re
-from os import rename
-from urllib.parse import urlparse, parse_qs
+import httplib2
 from jinja2 import Environment, FileSystemLoader
+from apiclient import discovery
+from oauth2client.service_account import ServiceAccountCredentials
+import os
 
-FACULTY_CSV_URL = 'https://docs.google.com/spreadsheets/export?format=csv&id=1WW7S0t6qUcFabLL4I9bE1uLex8wQmA40KfNEfXqCma4'
+
 OUTPUT_DIR = './_people/faculty-new/'
+# max number of position blocks (title and affiliation) that we allow on the 
+# Google form
 NUM_POSITION_BLOCKS_MAX = 4
+# max number of name fields that we allow on the Google form
 NUM_NAME_FIELDS_MAX = 5
+GOOGLE_SHEETS_ID = '1WW7S0t6qUcFabLL4I9bE1uLex8wQmA40KfNEfXqCma4'
+# the sheet name and column range to use
+GOOGLE_SHEETS_RANGE = 'Form Responses 1!A:X'
+GOOGLE_CREDENTIALS_PATH = 'secrets/google-api-credentials.json'
 
-# join together any number of strings, replacing all whitespace with sep,
-# converting to lowercase, and stripping out non-alphanumeric characters
+# TODO: pull this from a sequence file and keep it up to date
+LAST_ACCESSED_ROW = 1
+
 def normalize(fields, sep="_"):
+  """
+  Join together any number of strings, replacing all whitespace with sep,
+  converting to lowercase, and stripping out non-alphanumeric characters
+  """
   fields = [fields] if not type(fields) is list else fields
   regex = '[^A-Za-z0-9\\' + sep + ']'
   return re.sub(regex, '', sep.join(fields).lower().replace(' ', sep))
 
-# certain fields--like title, affiliation, and name--have a sequential index in
-# the header. This function pulls that index out for use
-def get_field_index(field):
+
+def pluck_field_index(field):
+  """
+  Certain fields--like title, affiliation, and name--have a sequential index in
+  the header. This function pulls that index out for use
+  """
   return int(field.split('_')[-1]) - 1
+
 
 @invoke.task()
 def build_faculty_profiles():
+  if not os.path.exists(GOOGLE_CREDENTIALS_PATH):
+    print('Error: Missing credentials file %s. Aborting.' % GOOGLE_CREDENTIALS_PATH)
+    return
+
+  # establish our Google API credentials and connect to our spreadsheet
+  scope = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+  credentials = ServiceAccountCredentials.from_json_keyfile_name(GOOGLE_CREDENTIALS_PATH, scope)
+  http = credentials.authorize(httplib2.Http())
+  discovery_url = ('https://sheets.googleapis.com/$discovery/rest?'
+                   'version=v4')
+  google_service = discovery.build('sheets', 'v4', http=http,
+                                   discoveryServiceUrl=discovery_url)
+
+  # fetch data from the spreadsheet
+  result = (google_service
+            .spreadsheets()
+            .values()
+            .get(spreadsheetId=GOOGLE_SHEETS_ID, range=GOOGLE_SHEETS_RANGE)
+            .execute()
+            .get('values', []))
+
+  headers = result[0]
+
+  # set up our jinja2 template
   env = Environment(
     loader=FileSystemLoader(searchpath='./_people'),
-    trim_blocks = True,
-    lstrip_blocks = True
+    trim_blocks=True,
+    lstrip_blocks=True
   )
   template = env.get_template('_template.j2')
-  csv_file = requests.get(FACULTY_CSV_URL).text.splitlines();
-  reader = csv.reader(csv_file, delimiter=',')
-  headers = next(reader)
 
-  # loop over all rows in the spreadsheet
-  for row in reader:
+  # build templates for any new data on the spreadsheet
+  new_templates = 0
+  start_row = LAST_ACCESSED_ROW + 1
+  for row in result[start_row:]:
 
     # context object to feed to the jinja2 template
     ctx = {
@@ -67,19 +107,27 @@ def build_faculty_profiles():
     }
 
     # add data for each field to our context object
-    for i in range(len(headers)):
+    for i in range(len(row)):
       header = normalize(headers[i])
+      val = row[i].strip()
 
-      # title and affiliation blocks need to be combined into a 'position'
-      # object, one for each set of titles and affiliations
+      # title and affiliation fields need to be combined into a 'position'
+      # block, one for each set of titles and affiliations
       if header.startswith('title') or header.startswith('affiliations'):
-        ctx['positions'][get_field_index(header)][header.split('_')[0]] = row[i].strip()
+        if val:
+          ctx['positions'][pluck_field_index(header)][header.split('_')[0]] = val
 
       # names can consist of several fields
       elif header.startswith('name'):
-        ctx['name'][get_field_index(header)] = row[i].strip()
+        if val:
+          ctx['name'][pluck_field_index(header)] = val
+
       else:
-        ctx[header] = row[i].strip()
+        ctx[header] = val
+
+    # filter out any unused position blocks and name fields
+    ctx['positions'] = [x for x in ctx['positions'] if x]
+    ctx['name'] = [x for x in ctx['name'] if x]
 
     for i in range(len(ctx['positions'])):
 
@@ -88,30 +136,30 @@ def build_faculty_profiles():
       ctx['positions'][i]['affiliations'] = \
         [affil.strip() for affil in ctx['positions'][i]['affiliations'].split(',')]
 
-    # remove any unused title/affiliation and name blocks
-    ctx['positions'].remove({'title': '', 'affiliations': ['']})
-    ctx['name'].remove('')
-
     outfile_base = normalize(ctx['name'], sep='-')
     with open(OUTPUT_DIR + outfile_base + '.md', 'w') as fhand:
       fhand.write(template.render(ctx))
       fhand.close()
 
-    # fetch and save raw profile image
-    if ctx['profile_picture']:
+    new_templates += 1
 
-      # reformat the google drive url for downloading
-      parsed_url = urlparse(ctx['profile_picture'])
-      parsed_qs = parse_qs(parsed_url.query)
-      file_id = parsed_qs['id'][0]
-      url = 'https://drive.google.com/uc?export=download&id=' + file_id
-      response = requests.get(url)
+    # TODO: update image fetching to use google-api-python-client
+    # # fetch and save raw profile image
+    # if ctx['profile_picture']:
 
-      if response.status_code == 200:
-        path = OUTPUT_DIR + outfile_base + '-raw'
-        with open(path, 'wb') as fhand:
-          fhand.write(response.content)
-          fhand.close()
-          file_type = imghdr.what(path)
-          rename(path, path + '.' + file_type)
+    #   # reformat the google drive url for downloading
+    #   parsed_url = urlparse(ctx['profile_picture'])
+    #   parsed_qs = parse_qs(parsed_url.query)
+    #   file_id = parsed_qs['id'][0]
+    #   url = 'https://drive.google.com/uc?export=download&id=' + file_id
+    #   response = requests.get(url)
+
+    #   if response.status_code == 200:
+    #     path = OUTPUT_DIR + outfile_base + '-raw'
+    #     with open(path, 'wb') as fhand:
+    #       fhand.write(response.content)
+    #       fhand.close()
+    #       file_type = imghdr.what(path)
+    #       rename(path, path + '.' + file_type)
           
+  print("Done! Built %d new faculty templates" % new_templates)
